60ff2f975c2ad48d5d85ee741c4a88ed9b5a2153
Felipe Guimarães <89321902+guimaraesfe1@users.noreply.github.com>
2026-04-11 20:56:02 -0300

    Define CRUD routes for task entity

--- src/routes/task.py
+++ src/routes/task.py
@@ -2,3 +2,23 @@ from fastapi import APIRouter
 
 
 router = APIRouter(prefix="/task")
+
+
+@router.post("")
+def create_task():
+    pass
+
+
+@router.get("/{task_id}")
+def get_task_by_id(task_id: int):
+    pass
+
+
+@router.delete("/{task_id}")
+def delete_task(task_id: int):
+    pass
+
+
+@router.put("/{task_id}")
+def update_task(task_id: int):
+    pass
